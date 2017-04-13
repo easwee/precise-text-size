47eb184ee59678a44f2951f8bcfe893a14bcca2a
a.gorkic <a.gorkic@sportradar.com>
2017-04-13 14:51:19 +0200

    Refactored code a bit to make it more readable.

--- precise-text-size.js
+++ precise-text-size.js
@@ -1,44 +1,46 @@
-// returns precise string width - read comments to understand how and what
 function getPreciseTextSize(targetId, debug) {
     var debug = debug || false;
-    var firstX = null;
-    var lastX = null;
-    var firstY = null;
-    var lastY = null;
-
     var el = document.getElementById(targetId);
     var style = window.getComputedStyle(el);
     var c = document.createElement("canvas");
-    var ctx = c.getContext("2d");
+    var ct = c.getContext("2d");
 
     var font = {
+        size:   parseInt(style.fontSize),
         family: style.fontFamily,
-        size: parseInt(style.fontSize),
         weight: style.fontWeight,
-        style: style.fontStyle
+        style:  style.fontStyle
+    };
+
+    var size = {
+        x: {
+            first: null,
+            last: null
+        },
+        y: {
+            first: null,
+            last: null
+        }
     };
 
     var spacing = font.size;
 
-
-    // uncomment next line to render canvas for debugging, we don't want to add canvas to dom and waste performance,
-    // we just use canvas API, which has handy methods for text calculations, all is done in memory without dom manipulation
     if(debug) {
         document.body.appendChild(c);
     }
 
-    ctx.font = font.style + " " + font.weight + " " + font.size + "px " + font.family;
+    ct.font = font.style + " " + font.weight + " " + font.size + "px " + font.family;
 
     var text = el.textContent;
-    var textSize = ctx.measureText(text);
+    var textSize = ct.measureText(text);
 
-    c.width = ctx.canvas.width = textSize.width + 2*spacing;
-    c.height = ctx.canvas.height = font.size + 2*spacing;
+    c.width  = ct.canvas.width = textSize.width + 2*spacing;
+    c.height = ct.canvas.height = font.size + 2*spacing;
 
-    ctx.font = font.style + " " + font.weight + " " + font.size + "px " + font.family;
-    ctx.fillText(text, 0, font.size + spacing);
+    ct.font = font.style + " " + font.weight + " " + font.size + "px " + font.family;
+    ct.fillText(text, 0, font.size + spacing);
 
-    var data = ctx.getImageData(0, 0, c.width, c.height).data;
+    var data = ct.getImageData(0, 0, c.width, c.height).data;
 
     for (var x = 0; x < c.width; x++) {
         for (var y = 0; y < c.height; y++) {
@@ -46,26 +48,26 @@ function getPreciseTextSize(targetId, debug) {
 
             if(data[index+3] > 0) {
 
-                if(firstY === null || y < firstY) {
-                    firstY = y;
+                if(size.y.first === null || y < size.y.first) {
+                    size.y.first = y;
                 }
 
-                if(firstX === null) {
-                    firstX = x ;
+                if(size.x.first === null) {
+                    size.x.first = x ;
                 }
 
-                lastX = x;
+                size.x.last = x;
 
-                if(lastY < y) {
-                    lastY = y;
+                if(size.y.last < y) {
+                    size.y.last = y;
                 }
             }
         }
     }
 
     return {
-        width: (lastX+1) - firstX, // add one because zero based
-        height: (lastY+1) - firstY,
-        offset: firstX
+        width:  (size.x.last+1) - size.x.first,
+        height: (size.y.last+1) - size.y.first,
+        offset: size.x.first
     }
 }
